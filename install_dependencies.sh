7006517a3bb5b7df9e8b336626fad12554dc33a9
gianlu9911 <49455217+gianlu9911@users.noreply.github.com>
2025-11-17 17:26:59 +0100

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,34 +1,33 @@
-#!/bin/bash
+# Aggiorna pip e installa openmim
+pip install --no-cache-dir --force-reinstall -U pip
+pip install --no-cache-dir --force-reinstall -U openmim
 
-# Exit script if any command fails
-set -e
+# Installazioni con MIM
+pip install --no-cache-dir --force-reinstall mmcv==2.1.0 -f https://download.openmmlab.com/mmcv/dist/cu118/torch2.1/index.html
+pip install --no-cache-dir --force-reinstall git+https://github.com/open-mmlab/mmdetection.git@v3.3.0
 
-# Update pip and install openmim
-echo "Updating pip and installing openmim..."
-pip install -U pip
-pip install -U openmim
+# Pacchetti Python generici
+pip install --no-cache-dir --force-reinstall supervision
+pip install --no-cache-dir --force-reinstall transformers==4.38.2
+pip install --no-cache-dir --force-reinstall nltk==3.8.1
+pip install --no-cache-dir --force-reinstall h5py
+pip install --no-cache-dir --force-reinstall einops
+pip install --no-cache-dir --force-reinstall seaborn
+pip install --no-cache-dir --force-reinstall fairscale
+pip install --no-cache-dir --force-reinstall git+https://github.com/openai/CLIP.git --no-deps
+pip install --no-cache-dir --force-reinstall git+https://github.com/siyuanliii/TrackEval.git
+pip install --no-cache-dir --force-reinstall git+https://github.com/SysCV/tet.git#subdirectory=teta
+pip install --no-cache-dir --force-reinstall git+https://github.com/scalabel/scalabel.git@scalabel-evalAPI
+pip install --no-cache-dir --force-reinstall git+https://github.com/TAO-Dataset/tao
+pip install --no-cache-dir --force-reinstall git+https://github.com/lvis-dataset/lvis-api.git
 
-# Install packages using MIM
-echo "Installing packages with MIM..."
-mim install mmengine
-pip install mmcv==2.1.0 -f https://download.openmmlab.com/mmcv/dist/cu118/torch2.1/index.html
-pip install git+https://github.com/open-mmlab/mmdetection.git@v3.3.0
+# OpenCV e numpy (versione iniziale)
+pip install --no-cache-dir --force-reinstall opencv-python==4.11.0.86
+pip install --no-cache-dir --force-reinstall numpy==1.26.4
 
-# Install various Python packages using pip
-echo "Installing various Python packages..."
-pip install numpy==1.26.4
-pip install supervision
-pip install transformers==4.38.2
-pip install nltk==3.8.1
-pip install h5py
-pip install einops
-pip install seaborn
-pip install fairscale
-pip install git+https://github.com/openai/CLIP.git --no-deps
-pip install git+https://github.com/siyuanliii/TrackEval.git
-pip install git+https://github.com/SysCV/tet.git#subdirectory=teta
-pip install git+https://github.com/scalabel/scalabel.git@scalabel-evalAPI
-pip install git+https://github.com/TAO-Dataset/tao
-pip install git+https://github.com/lvis-dataset/lvis-api.git
+# PyTorch e moduli correlati (installati alla fine)
+pip install --no-cache-dir --force-reinstall torch==2.1.2 torchvision==0.16.2 torchaudio==2.1.2 --index-url https://download.pytorch.org/whl/cu118
 
-echo "All packages installed successfully!"
+# numpy versione finale
+pip install --no-cache-dir --force-reinstall numpy==1.26.4
+pip install --no-cache-dir --force-reinstall mdet
